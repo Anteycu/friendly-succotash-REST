8906b6a9d5882e3f1f54fe4f97df3cacdf4de1e2
Nikolay Trunov <anteycu@gmail.com>
2022-01-30 18:56:18 +0200

    fix CORS problem

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const feedbackRouter = require("./routes/feedback.routes");
+const corsMiddleware = require("./middleware/middleware");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(corsMiddleware);
 app.use(express.json());
 app.use("/api", feedbackRouter);
 
--- routes/feedback.routes.js
+++ routes/feedback.routes.js
@@ -6,5 +6,8 @@ router.post("/feedback", feedbackController.createFeedback);
 router.get("/feedback", feedbackController.getFeedbacks);
 router.get("/feedback/:id", feedbackController.getFeedback);
 router.delete("/feedback/:id", feedbackController.deleteFeedback);
+router.options("/*", (_, res) => {
+  res.sendStatus(200);
+});
 
 module.exports = router;
